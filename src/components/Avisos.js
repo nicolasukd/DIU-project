f45fa42cbf61035f3814196a9f72acbc2896503e
nicolasukd <nicolas.jorquera@sansano.usm.cl>
2023-11-16 18:44:49 -0300

    chatAdmin

--- src/components/Avisos.js
+++ src/components/Avisos.js
@@ -2,80 +2,114 @@ import '../assets/styles/Avisos.css'
 import feria from '../assets/images/feriaNavideña.jpg'
 
 
-import { Card, Button } from 'flowbite-react';
+import { Card, Button, Modal } from 'flowbite-react';
+import { useState } from 'react';
 
 
 
 function Avisos() {
 
+    const [openModal, setOpenModal] = useState(false);
+
     return (
-        <div className='container'>
-            <div className='row'>
-                <Card className="max-w-sm" imgSrc={feria} horizontal>
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Noteworthy technology acquisitions 2021
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                    </p>
-                </Card>
-            </div>
+        <>
+            <div className='container'>
+                <div className='row'>
+                    <Card className="max-w-sm" imgSrc={feria} horizontal onClick={() => setOpenModal(true)}>
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Noteworthy technology acquisitions 2021
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        </p>
+                    </Card>
+                </div>
 
-            <div className='row'>
-                <Card
-                    className="max-w-sm"
-                    imgAlt="Meaningful alt text for an image that is not purely decorative"
-                    imgSrc={feria}
-                >
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Feria navideña
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                    </p>
-                </Card>
-                <Card
-                    className="max-w-sm"
-                    imgAlt="Meaningful alt text for an image that is not purely decorative"
-                    imgSrc={feria}
-                >
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Feria navideña
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                    </p>
-                </Card>
-            </div>
+                <div className='row'>
+                    <Card
+                        className="max-w-sm"
+                        imgAlt="Meaningful alt text for an image that is not purely decorative"
+                        imgSrc={feria}
+                        onClick={() => setOpenModal(true)}
+                    >
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Feria navideña
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        </p>
+                    </Card>
+                    <Card
+                        className="max-w-sm"
+                        imgAlt="Meaningful alt text for an image that is not purely decorative"
+                        imgSrc={feria}
+                        onClick={() => setOpenModal(true)}
+                    >
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Feria navideña
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        </p>
+                    </Card>
+                </div>
 
-            <div className='row'>
-                <Card
-                    className="max-w-sm"
-                    imgAlt="Meaningful alt text for an image that is not purely decorative"
-                    imgSrc={feria}
-                >
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Feria navideña
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                    </p>
-                </Card>
-                <Card
-                    className="max-w-sm"
-                    imgAlt="Meaningful alt text for an image that is not purely decorative"
-                    imgSrc={feria}
-                >
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Feria navideña
-                    </h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                    </p>
-                </Card>
-            </div>
+                <div className='row'>
+                    <Card
+                        className="max-w-sm"
+                        imgAlt="Meaningful alt text for an image that is not purely decorative"
+                        imgSrc={feria}
+                        onClick={() => setOpenModal(true)}
+                    >
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Feria navideña
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        </p>
+                    </Card>
+                    <Card
+                        className="max-w-sm"
+                        imgAlt="Meaningful alt text for an image that is not purely decorative"
+                        imgSrc={feria}
+                        onClick={() => setOpenModal(true)}
+                    >
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Feria navideña
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        </p>
+                    </Card>
+                </div>
 
-        </div>
+            </div>
+            <Modal dismissible show={openModal} onClose={() => setOpenModal(false)} on>
+                <Modal.Header>Detalles del aviso</Modal.Header>
+                <Modal.Body>
+                    <div className="space-y-6">
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                            companies around the world are updating their terms of service agreements to comply.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
+                            to ensure a common set of data rights in the European Union. It requires organizations to notify users as
+                            soon as possible of high-risk data breaches that could personally affect them.
+                        </p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                <div className="space-y-6">
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            12/12/2021 12:00   
+                        </p>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </>
+
+
     )
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,53 +5,85 @@ import '../assets/styles/Sidebar.css'
 import logo from '../assets/images/logo.png'
 
 import { Outlet, Link } from "react-router-dom";
-import { Sidebar, Button } from 'flowbite-react';
+import * as React from 'react';
+import { Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup';
+import { Sidebar, Button, Card, Label, TextInput, Checkbox } from 'flowbite-react';
 import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
 
 function Component() {
+    const [anchor, setAnchor] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchor(anchor ? null : event.currentTarget);
+    };
+
+    const open = Boolean(anchor);
+    const id = open ? 'simple-popup' : undefined;
+
     return (
-        <Sidebar aria-label="Default sidebar example" >
-            <Sidebar.Logo href="#" img={logo} imgAlt="logo">
-                App
-            </Sidebar.Logo>
-            <div className='Sidebar'>
-                <Sidebar.Items style={{ textAlign: 'initial' }}>
-                    <Sidebar.ItemGroup>
-                        <Sidebar.Item href="/avisos" icon={HiChartPie}>
-                            Avisos
-                        </Sidebar.Item>
-                        <Sidebar.Item href="/gastosComunes" icon={HiViewBoards} label="3" >
-                            Gastos comúnes
-                        </Sidebar.Item>
-                        <Sidebar.Item href="/visitas" icon={HiInbox}>
-                            Visitas
-                        </Sidebar.Item>
-                        <Sidebar.Item href="/espaciosComunes" icon={HiUser}>
-                            Espacios comúnes
-                        </Sidebar.Item>
-                        <Sidebar.Item href="#" icon={HiShoppingBag}>
-                            Otra
-                        </Sidebar.Item>
-                        <Sidebar.Item href="#" icon={HiArrowSmRight}>
-                            Otra
-                        </Sidebar.Item>
-                    </Sidebar.ItemGroup>
-                    <Sidebar.ItemGroup>
-                        <Sidebar.Item href="#" icon={HiChartPie}>
-                            Ajustes
-                        </Sidebar.Item>
-                        <Sidebar.Item href="#" icon={HiViewBoards}>
-                            Suscripción
-                        </Sidebar.Item>
-                    </Sidebar.ItemGroup>
-                </Sidebar.Items>
-                <Sidebar.CTA>
-                    <Button color='failure'>
-                        Hablar con administración
-                    </Button>
-                </Sidebar.CTA>
-            </div>
-        </Sidebar>
+        <div>
+            <Sidebar aria-label="Default sidebar example" >
+                <Sidebar.Logo href="#" img={logo} imgAlt="logo">
+                    App
+                </Sidebar.Logo>
+                <div className='Sidebar'>
+                    <Sidebar.Items style={{ textAlign: 'initial' }}>
+                        <Sidebar.ItemGroup>
+                            <Sidebar.Item href="/avisos" icon={HiChartPie}>
+                                Avisos
+                            </Sidebar.Item>
+                            <Sidebar.Item href="/gastosComunes" icon={HiViewBoards} label="3" >
+                                Gastos comúnes
+                            </Sidebar.Item>
+                            <Sidebar.Item href="/visitas" icon={HiInbox}>
+                                Visitas
+                            </Sidebar.Item>
+                            <Sidebar.Item href="/espaciosComunes" icon={HiUser}>
+                                Espacios comúnes
+                            </Sidebar.Item>
+                            <Sidebar.Item href="#" icon={HiShoppingBag}>
+                                Otra
+                            </Sidebar.Item>
+                            <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                                Otra
+                            </Sidebar.Item>
+                        </Sidebar.ItemGroup>
+                        <Sidebar.ItemGroup>
+                            <Sidebar.Item href="#" icon={HiChartPie}>
+                                Ajustes
+                            </Sidebar.Item>
+                            <Sidebar.Item href="#" icon={HiViewBoards}>
+                                Suscripción
+                            </Sidebar.Item>
+                        </Sidebar.ItemGroup>
+                    </Sidebar.Items>
+                    <Sidebar.CTA>
+                        <Button color='failure' aria-describedby={id} type="button" onClick={handleClick}>
+                            Hablar con administración
+                        </Button>
+                    </Sidebar.CTA>
+
+                </div>
+            </Sidebar>
+            <BasePopup id={id} open={open} anchor={anchor} placement={'right-end'} style={{ left: '50px' }}>
+                <Card className="max-w-sm">
+                    <form className="flex flex-col gap-4">
+                        <div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="email1" value="Administración" />
+                            </div>
+                            <TextInput style={{height:'200px'}} id="large" type="email" sizing={'lg'} />
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <TextInput id="large" type="text" />
+                            <Button color="failure">Enviar</Button>
+                        </div>
+
+                    </form>
+                </Card>
+            </BasePopup>
+        </div>
+
     );
 }
 
